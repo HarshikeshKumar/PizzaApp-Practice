3d1801c76b21962288ccd15ee056c5ee40b413b1
HarshikeshKumar <harshikeshkoili214@gmail.com>
2026-03-07 22:52:04 +0530

    3-Registration, 'TOKEN STORE IN httpOnly Cookie and ACCESS TOKEN', LOGIN/SIGNIN Implementation end-to-end done, Iss commit me maine Token ko httpOnlyCookie me store kiya hai aur console me Token ko ACCESS bhi kiya hai

--- 3-Registration/src/controllers/authController.js
+++ 3-Registration/src/controllers/authController.js
@@ -6,10 +6,18 @@ async function loginController(req, res) {
     const loginPayload = req.body;
     const response = await loginUser(loginPayload);
 
+    // Set Token in httpOnly cookie
+    res.cookie("authToken", response, {
+      httpOnly: true,
+      success: false,
+      maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days ko milisec me convert kiya
+    });
+
     return res.status(200).json({
       success: true,
       message: "Logged in successfully",
-      data: response, // data me token bhej dunga
+      // data: response, // data me token bhej dunga
+      data: {},
       error: {},
     });
   } catch (error) {
--- 3-Registration/src/index.js
+++ 3-Registration/src/index.js
@@ -4,6 +4,7 @@ import userRouter from "./routes/userRoute.js";
 import cartRouter from "./routes/cartRoute.js";
 import connectDB from "./config/dbConfig.js";
 import authRouter from "./routes/authRoute.js";
+import cookieParser from "cookie-parser";
 
 const app = express();
 
@@ -11,11 +12,17 @@ app.use(express.json());
 app.use(express.text());
 app.use(express.urlencoded({ extended: true }));
 
+// Access cookie in backend
+app.use(cookieParser());
+
 app.use("/users", userRouter);
 app.use("/carts", cartRouter);
 app.use("/auth", authRouter);
 
 app.get("/ping", (req, res) => {
+  console.log(req.body);
+  // Cookie ko access krr raha hu check krne ke liye access ho raha hai ya nhi
+  console.log(req.cookies);
   return res.json({
     message: "Pong",
   });
