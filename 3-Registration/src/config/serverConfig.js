78d404abd48508f4e95adf56d1f4051fd965a361
HarshikeshKumar <harshikeshkoili214@gmail.com>
2026-03-12 22:29:42 +0530

    3-Registration, Step:1 for Cloudinary  'ADDED CLOUDINARY CONFIG'

--- 3-Registration/src/config/serverConfig.js
+++ 3-Registration/src/config/serverConfig.js
@@ -2,6 +2,8 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
+// Here we are exporting all the env variables thet the project uses
+
 export const PORT = process.env.PORT;
 
 export const MONGO_URL = process.env.MONGO_URL;
@@ -9,3 +11,9 @@ export const MONGO_URL = process.env.MONGO_URL;
 export const JWT_SECRET = process.env.JWT_SECRET;
 
 export const JWT_EXPIRY = process.env.JWT_EXPIRY;
+
+export const CLOUDINARY_CLOUD_NAME = process.env.CLOUDINARY_CLOUD_NAME;
+
+export const CLOUDINARY_API_KEY = process.env.CLOUDINARY_API_KEY;
+
+export const CLOUDINARY_API_SECRET = process.env.CLOUDINARY_API_SECRET;
