82a3914ebefe770b4be4b2b7dd3ebddc3c67d837
HarshikeshKumar <harshikeshkoili214@gmail.com>
2026-03-13 01:10:15 +0530

    3-Registration, Iss commit me 'productSchema.js' ka bugs fix kiya

--- 3-Registration/src/schema/productSchema.js
+++ 3-Registration/src/schema/productSchema.js
@@ -25,7 +25,7 @@ const productSchema = new mongoose.Schema(
 
     category: {
       type: String,
-      enum: ["veg, non-veg, drinks, sides"], // In 4 variable me se hi koi ek daal paoge
+      enum: ["veg", "non-veg", "drinks", "sides"], // In 4 variable me se hi koi ek daal paoge
       default: "veg",
     },
 
